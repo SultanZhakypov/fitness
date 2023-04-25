d3176644a75cedc2196d818c31b8fa4def10f3f5
kyrgyz00 <115091186+kyrgyz00@users.noreply.github.com>
2023-04-25 13:11:32 +0600

    changed smth

---
+++ lib/features/screens/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePageScreen extends StatelessWidget {
+  const HomePageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: 100,
+        height: 100,
+      ),
+    );
+  }
+}
