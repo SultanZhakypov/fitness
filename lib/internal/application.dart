81a827fcb7d564178d6c1ab0a7783e3e0e630500
Esen888 <esensharsheev808@gmail.com>
2023-04-25 14:00:06 +0600

    Fix errors

--- lib/internal/application.dart
+++ lib/internal/application.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../features/screens/home_page.dart';
 
 class MyApp extends StatelessWidget {
