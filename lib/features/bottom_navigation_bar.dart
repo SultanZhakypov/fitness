ebb37088c90f73e6b7b4b384dabe0ec596e0a177
Esen888 <esensharsheev808@gmail.com>
2023-04-25 15:44:15 +0600

    Add BottomNavBar

--- lib/features/bottom_navigation_bar.dart
+++ lib/features/bottom_navigation_bar.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -9,8 +10,39 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedTab = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text("HOME"),
+    Text("DESK"),
+    Text("NEWS"),
+  ];
+
+  void onSelectTab(int index) {
+    if (_selectedTab == index) return;
+
+    setState(() {
+      _selectedTab = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedTab,
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.white,
+          backgroundColor: Color(0xff8A8A8A),
+          onTap: onSelectTab,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "HOME"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_today), label: "DESK"),
+            BottomNavigationBarItem(icon: Icon(Icons.newspaper), label: "NEWS"),
+          ]),
+      body: Center(
+        child: _widgetOptions[_selectedTab],
+      ),
+    );
   }
 }
