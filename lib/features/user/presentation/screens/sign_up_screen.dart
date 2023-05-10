226ddc57d362f2db17b78339f1bc4e66a627ccd7
Esen888 <esensharsheev808@gmail.com>
2023-05-10 13:42:06 +0600

    fixed bugs sign screen

--- lib/features/user/presentation/screens/sign_up_screen.dart
+++ lib/features/user/presentation/screens/sign_up_screen.dart
@@ -56,100 +56,106 @@ class SignUpScreen extends StatelessWidget {
         ),
       ),
       backgroundColor: ColorHelper.backgroundColor,
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: EdgeInsets.all(20.r),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(top: 40.h, bottom: 15.h),
-                child: Text(
-                  'Имя',
-                  style: TextHelper.w700s20
-                      .copyWith(color: ColorHelper.greyD1D3D3),
-                ),
-              ),
-              SignUpTextFieldCard(
-                controller: controller.nickName,
-                error: 'е имя'.toLowerCase(),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 20.h, bottom: 15.h),
-                child: Text(
-                  'Email',
-                  style: TextHelper.w700s20
-                      .copyWith(color: ColorHelper.greyD1D3D3),
-                ),
-              ),
-              SignUpTextFieldCard(
-                controller: controller.email,
-                error: ' Email'.toLowerCase(),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 20.h, bottom: 15.h),
-                child: Text(
-                  'Номер телефона',
-                  style: TextHelper.w700s20
-                      .copyWith(color: ColorHelper.greyD1D3D3),
-                ),
-              ),
-              SignUpTextFieldCard(
-                controller: controller.phoneNumber,
-                textInputType: TextInputType.phone,
-                error: ' Номер телефона'.toLowerCase(),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 20.h, bottom: 15.h),
-                child: Text(
-                  'Пароль',
-                  style: TextHelper.w700s20
-                      .copyWith(color: ColorHelper.greyD1D3D3),
-                ),
-              ),
-              PasswordSignUpTextFieldCard(
-                controller: controller.password,
-              ),
-              SizedBox(height: 40.h),
-              InkWell(
-                onTap: () {
-                  if (_formKey.currentState!.validate()) {
-                    final user = UserModel(
-                      nickName: controller.nickName.text.trim(),
-                      email: controller.email.text.trim(),
-                      phoneNumber: controller.phoneNumber.text.trim(),
-                      password: controller.password.text.trim(),
-                    );
-                    SignUpController.instance.createUser(user);
-                  }
-                },
-                child: Container(
-                  width: 1.sw,
-                  height: 60.h,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(color: Colors.purple.shade900, blurRadius: 6)
-                    ],
-                    borderRadius: BorderRadius.circular(15),
-                    gradient: LinearGradient(
-                      colors: [
-                        ColorHelper.blue01DDEB,
-                        Colors.blue,
-                      ],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
+      body: SafeArea(
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(20.r),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 40.h, bottom: 15.h),
+                    child: Text(
+                      'Имя',
+                      style: TextHelper.w700s20
+                          .copyWith(color: ColorHelper.greyD1D3D3),
                     ),
                   ),
-                  child: Text(
-                    'Зарегистрироваться',
-                    style: TextHelper.w700s20
-                        .copyWith(color: ColorHelper.greyD1D3D3),
+                  SignUpTextFieldCard(
+                    controller: controller.nickName,
+                    error: 'е имя'.toLowerCase(),
                   ),
-                ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20.h, bottom: 15.h),
+                    child: Text(
+                      'Email',
+                      style: TextHelper.w700s20
+                          .copyWith(color: ColorHelper.greyD1D3D3),
+                    ),
+                  ),
+                  SignUpTextFieldCard(
+                    controller: controller.email,
+                    textInputType: TextInputType.emailAddress,
+                    error: ' Email'.toLowerCase(),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20.h, bottom: 15.h),
+                    child: Text(
+                      'Номер телефона',
+                      style: TextHelper.w700s20
+                          .copyWith(color: ColorHelper.greyD1D3D3),
+                    ),
+                  ),
+                  SignUpTextFieldCard(
+                    controller: controller.phoneNumber,
+                    textInputType: TextInputType.phone,
+                    error: ' Номер телефона'.toLowerCase(),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20.h, bottom: 15.h),
+                    child: Text(
+                      'Пароль',
+                      style: TextHelper.w700s20
+                          .copyWith(color: ColorHelper.greyD1D3D3),
+                    ),
+                  ),
+                  PasswordSignUpTextFieldCard(
+                    controller: controller.password,
+                  ),
+                  SizedBox(height: 40.h),
+                  InkWell(
+                    onTap: () {
+                      if (_formKey.currentState!.validate()) {
+                        final user = UserModel(
+                          nickName: controller.nickName.text.trim(),
+                          email: controller.email.text.trim(),
+                          phoneNumber: controller.phoneNumber.text.trim(),
+                          password: controller.password.text.trim(),
+                        );
+                        SignUpController.instance.createUser(user);
+                      }
+                    },
+                    child: Container(
+                      width: 1.sw,
+                      height: 60.h,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.purple.shade900, blurRadius: 6)
+                        ],
+                        borderRadius: BorderRadius.circular(15),
+                        gradient: LinearGradient(
+                          colors: [
+                            ColorHelper.blue01DDEB,
+                            Colors.blue,
+                          ],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
+                      ),
+                      child: Text(
+                        'Зарегистрироваться',
+                        style: TextHelper.w700s20
+                            .copyWith(color: ColorHelper.greyD1D3D3),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
