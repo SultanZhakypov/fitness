614b5612e8f1fd7f809e9ebbde580321722df974
kyrgyz00 <esensharsheev808@gmail.com>
2023-05-13 19:44:13 +0600

    fixed my name

--- lib/internal/helpers/text_helper.dart
+++ lib/internal/helpers/text_helper.dart
@@ -1,35 +1,35 @@
 import 'package:flutter/material.dart';
 
 class TextHelper {
-  static TextStyle w700s20 = TextStyle(
+  static TextStyle w700s20 = const TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.w700,
   );
-  static TextStyle w700s18 = TextStyle(
+  static TextStyle w700s18 = const TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w700,
   );
-  static TextStyle w700s15 = TextStyle(
+  static TextStyle w700s15 = const TextStyle(
     fontSize: 15,
     fontWeight: FontWeight.w700,
   );
-  static TextStyle w700s10 = TextStyle(
+  static TextStyle w700s10 = const TextStyle(
     fontSize: 10,
     fontWeight: FontWeight.w700,
   );
-  static TextStyle w700s16 = TextStyle(
+  static TextStyle w700s16 = const TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w700,
   );
-  static TextStyle w400s11 = TextStyle(
+  static TextStyle w400s11 = const TextStyle(
     fontSize: 11,
     fontWeight: FontWeight.w400,
   );
-  static TextStyle w700s11 = TextStyle(
+  static TextStyle w700s11 = const TextStyle(
     fontSize: 11,
     fontWeight: FontWeight.w700,
   );
-   static TextStyle w700s23 = TextStyle(
+   static TextStyle w700s23 = const TextStyle(
     fontSize: 23,
     fontWeight: FontWeight.w700,
   );
