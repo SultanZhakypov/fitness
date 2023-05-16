b59e497b46cb7229e91c2e37d6d7bb8c7e1b4c28
kyrgyz00 <bekbolottashpolotov@gmail.com>
2023-05-16 14:26:39 +0600

    fixed lottie

--- lib/features/blogger/presentation/widgets/blogger_card.dart
+++ lib/features/blogger/presentation/widgets/blogger_card.dart
@@ -7,8 +7,6 @@ import 'package:BodyPower/internal/helpers/text_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
-
 import '../../../../internal/helpers/color_helper.dart';
 
 class BloggerCard extends StatelessWidget {
